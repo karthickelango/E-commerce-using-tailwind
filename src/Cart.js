3bb1bdf2d1900a3ad207c8e2fd683533ea352f9e
karthickelango <karthik5ive@gmail.com>
2023-08-28 17:08:34 +0530

    add style

--- src/Cart.js
+++ src/Cart.js
@@ -14,19 +14,84 @@ const Cart = () => {
           }
   return (
     <main className='mt-150'>
-      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        { products.length > 0 ?
-        <div>
-        {products.map(product => (
-           <> 
-                <img src={product.imageSrc} id={product.id}></img>
-                <button onClick={() => removeItem(product.id)}>Remove</button>
-        </>
-            
-        ))}
-        </div> : 
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-          {/* <img className='emptyCart' src={Loader}/> */}
+      {products.length > 0 ?
+      <div className="mx-auto max-w-2xl px-4 sm:px-6  lg:px-8">
+      {products.map(product => (
+      <div class="flex font-sans border-1 mb-40 p-20 br-5">
+  <div class="flex-none w-48 relative">
+          <img src={product.imageSrc} id={product.id} class="absolute inset-0 w-full h-full object-cover" loading="lazy" />
+          <button onClick={() => removeItem(product.id)}>Remove</button>
+  </div>
+  <form class="flex-auto p-6">
+    <div class="flex flex-wrap">
+      <h1 class="flex-auto text-lg font-semibold text-slate-900">
+        {product.name}
+      </h1>
+      <div class="text-lg font-semibold text-slate-500">
+        {product.price}
+      </div>
+      <div class="w-full flex-none text-sm font-medium text-slate-700 mt-2">
+        In stock
+      </div>
+    </div>
+    <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+      <div class="space-x-2 flex text-sm">
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="xs" />
+          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+            XS
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="s" checked />
+          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+            S
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="m" />
+          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+            M
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="l" />
+          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+            L
+          </div>
+        </label>
+        <label>
+          <input class="sr-only peer" name="size" type="radio" value="xl" />
+          <div class="w-9 h-9 rounded-lg flex items-center justify-center text-slate-700 peer-checked:font-semibold peer-checked:bg-slate-900 peer-checked:text-white">
+            XL
+          </div>
+        </label>
+      </div>
+    </div>
+    <div class="flex space-x-4 mb-6 text-sm font-medium">
+      <div class="flex-auto flex space-x-4">
+        {/* <button class="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+          Buy now
+        </button> */}
+        <button class="h-10 px-6 font-semibold rounded-md border border-slate-200 text-slate-900" type="button"
+                onClick={() => removeItem(product.id)}>
+          Remove from bag
+        </button>
+      </div>
+      {/* <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200" type="button" aria-label="Like">
+        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </button> */}
+    </div>
+    {/* <p class="text-sm text-slate-700">
+      Free shipping on all continental US orders.
+    </p> */}
+  </form>
+</div>
+))}
+</div> : 
+<main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
           <div className="text-center">
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Your cart is empty!</h1>
             <p className="mt-6 text-base leading-7 text-gray-600">Add items to it now.</p>
@@ -36,9 +101,8 @@ const Cart = () => {
               </Link>
             </div>
           </div>
-        </main> 
-        }
-        </div>
+        </main>
+}
     </main>
     
   )
