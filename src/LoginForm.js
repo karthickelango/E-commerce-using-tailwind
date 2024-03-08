d020585d43599977e3817d8269d2a58ad7a0a32b
karthickelango <karthik5ive@gmail.com>
2024-03-08 13:00:28 +0530

    add style

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -59,9 +59,9 @@ const LoginForm = () => {
                                                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
                                                     <div className="w-64">
-                                                        <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Mobile number</label>
+                                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Mobile number</label>
                                                         <div className="mt-2">
-                                                            <input id="number" name="number" type="number" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            <input id="number" name="number" type="number" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -92,9 +92,9 @@ const LoginForm = () => {
                                                 <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
                                                     <div className="w-64">
-                                                        <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Mobile number</label>
+                                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Mobile number</label>
                                                         <div className="mt-2">
-                                                            <input id="number" name="number" type="number" autocomplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                                            <input id="number" name="number" type="number" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                                         </div>
                                                     </div>
                                                 </div>
